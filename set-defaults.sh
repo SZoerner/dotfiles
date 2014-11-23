aae2577ebf086631232313d7cdf914f26e638521
Sebastian Zoerner <zoerner7@gmail.com>
2014-11-23 17:25:19 +0100

    dock and mission control

--- set-defaults.sh
+++ set-defaults.sh
@@ -37,10 +37,6 @@ chflags nohidden ~/Library
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
-
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
@@ -119,6 +115,10 @@ echo ""
 echo "Setting trackpad & mouse speed to a reasonable number"
 defaults write -g com.apple.trackpad.scaling 2
 defaults write -g com.apple.mouse.scaling 2.5
+
+echo ""
+echo "automatically illuminate built-in MacBook keyboard in low light"
+defaults write com.apple.BezelServices kDim -bool true
  
 echo ""
 echo "Turn off keyboard illumination when computer is not used for 5 minutes"
@@ -190,29 +190,92 @@ echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 
-###############################################################################
-# Dock & Mission Control
-###############################################################################
+############################################################################
+# Dock, Dashboard, and hot corners                                         #
+############################################################################
+
 
 # Wipe all (default) app icons from the Dock
 # This is only really useful when setting up a new Mac, or if you donÃ¢â‚¬â„¢t use
 # the Dock to launch apps.
-#defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-apps -array
 
-echo ""
-echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+# Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
 defaults write com.apple.dock tilesize -int 36
 
-echo ""
-echo "Speeding up Mission Control animations and grouping windows by application"
+# Dock: enable magnification
+defaults write com.apple.dock magnification -bool true
+
+# Set magnification icon size to 80 pixels
+defaults write com.apple.dock largesize -float 80
+
+# Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# grouping windows by application"
 defaults write com.apple.dock "expose-group-by-app" -bool true
 
-echo ""
-echo "Setting Dock to auto-hide and removing the auto-hiding delay"
+# Setting Dock to auto-hide and removing the auto-hiding delay"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
-defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.3
+
+# Run the screensaver if we're in the bottom-left hot corner.
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Dock: enable highlight hover effect for the grid view of a stack
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Dock: minimize windows into their application's icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Dock: enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Dock: enable animation when opening applications
+defaults write com.apple.dock launchanim -bool true
+
+# General: enable Dashboard as an overlay
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# General: automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool true
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Dock: make icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+# Top left screen corner → Dashboard
+#defaults write com.apple.dock wvous-tl-corner -int 7
+#defaults write com.apple.dock wvous-tl-modifier -int 0
+
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom left screen corner → Mission Control
+#defaults write com.apple.dock wvous-bl-corner -int 2
+#defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Bottom right screen corner → Launchpad
+#defaults write com.apple.dock wvous-br-corner -int 11
+#defaults write com.apple.dock wvous-br-modifier -int 0
 
 
 ###############################################################################
